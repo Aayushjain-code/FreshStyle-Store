86dcf233515a7ae3f292a4e553176480c8620e9d
Aayush Jain <75216351+Aayushjain-code@users.noreply.github.com>
2022-02-20 20:44:38 +0530

    bugs fixed

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ let navbar = document.querySelector('.navbar')
 
 document.querySelector('#menu-btn').onclick = () => {
     navbar.classList.toggle('active');
-    loginForm.classList.remove('active');
     searchForm.classList.remove('active');
 }
 
@@ -12,12 +11,6 @@ document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle('active');
     navbar.classList.remove('active');
     loginForm.classList.remove('active');
-}
-
-window.onscroll = () => {
-    navbar.classList.remove('active');
-    loginForm.classList.remove('active');
-    searchForm.classList.remove('active');
 }
 
 let themeBtn = document.querySelector('#theme-btn');
